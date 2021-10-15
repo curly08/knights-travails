7cf86b5fed595c92d3fb5419ab946aeb8afa6fea
curly08 <mattynoakes08@gmail.com>
2021-10-14 22:40:09 -0400

    Add check_possible_moves method and make_square_coordinates method

--- main.rb
+++ main.rb
@@ -2,24 +2,12 @@
 
 # Board class
 class Board
-  attr_accessor :squares
+  attr_accessor :squares, :knight
 
   def initialize
     @grid = make_grid
     @knight = Knight.new
   end
-
-  # def make_square_coordinates
-  #   rows = [0, 1, 2, 3, 4, 5, 6, 7]
-  #   columns = [7, 6, 5, 4, 3, 2, 1, 0]
-  #   squares = []
-  #   columns.each do |y|
-  #     rows.each do |x|
-  #       squares.push([x, y])
-  #     end
-  #   end
-  #   squares
-  # end
 
   def make_grid
     rows = [0, 1, 2, 3, 4, 5, 6, 7]
@@ -48,12 +36,43 @@ end
 
 # Knight class
 class Knight
-  attr_accessor :position
+  attr_accessor :position, :squares
 
   def initialize
     @position = [0, 0]
+    @squares = make_square_coordinates
+  end
+
+  def check_possible_moves(position = @position)
+    moves = [
+      [(position[0] + 1), (position[1] + 2)],
+      [(position[0] + 2), (position[1] + 1)],
+      [(position[0] + 2), (position[1] - 1)],
+      [(position[0] + 1), (position[1] - 2)],
+      [(position[0] - 1), (position[1] - 2)],
+      [(position[0] - 2), (position[1] - 1)],
+      [(position[0] - 2), (position[1] + 1)],
+      [(position[0] - 1), (position[1] + 2)]
+    ]
+    possible_moves = []
+    moves.each do |move|
+      possible_moves.push(move) if squares.include?(move)
+    end
+    possible_moves
+  end
+
+  def make_square_coordinates
+    rows = [0, 1, 2, 3, 4, 5, 6, 7]
+    columns = [7, 6, 5, 4, 3, 2, 1, 0]
+    squares = []
+    columns.each do |y|
+      rows.each do |x|
+        squares.push([x, y])
+      end
+    end
+    squares
   end
 end
 
 board = Board.new
-p board
+p board.knight.check_possible_moves
